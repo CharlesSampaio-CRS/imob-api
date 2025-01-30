4aa9ee5a953b58a85a19b21580830322d6ad54ca
Charles Sampaio <crsampaio@uolinc.com>
2025-01-30 08:31:55 -0300

    Add s3 upload files TenantService correction

--- src/main/kotlin/com/payloc/imob/constants/Constants.kt
+++ src/main/kotlin/com/payloc/imob/constants/Constants.kt
@@ -4,6 +4,7 @@ class Constants {
     companion object {
         const val ITEM_ALREADY_EXISTS = "Item already exists "
         const val ITEM_NOT_FOUND = "Item not found "
+        const val PREFIX_FILE = "imob_file_"
         const val INITIAL_ELEMENT_NUMBER = 1000001
     }
 }
