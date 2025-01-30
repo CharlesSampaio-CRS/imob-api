d4d9acb3b20514b001855a9b33d9537f6f61de04
Charles Sampaio <crsampaio@uolinc.com>
2025-01-30 09:16:03 -0300

    Add s3 upload files Property

--- src/main/kotlin/com/payloc/imob/constants/Constants.kt
+++ src/main/kotlin/com/payloc/imob/constants/Constants.kt
@@ -4,7 +4,6 @@ class Constants {
     companion object {
         const val ITEM_ALREADY_EXISTS = "Item already exists "
         const val ITEM_NOT_FOUND = "Item not found "
-        const val PREFIX_FILE = "imob_file_"
         const val INITIAL_ELEMENT_NUMBER = 1000001
     }
 }
